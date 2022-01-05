3b004ea55d55fffc91143e8e279ef43cc054f70d
Michael Knutson <mrknutson44@gmail.com>
2022-01-05 08:56:31 -0900

    getting agentProperties data for available page

--- client/src/pages/Available.js
+++ client/src/pages/Available.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap';
 
 const Available = (props) =>{
+  const [agentProperties, setAgentProperties] = useState([]);
+
+  useEffect(()=>{
+    getAgentProperties();
+  },[])
+
+  const getAgentProperties = async () => {
+    try{
+      let response = await axios.get(`/api/properties`)
+      console.log(response.data)
+      setAgentProperties(response.data)
+    } catch (err) {
+      alert('error getting agent properties')
+    };
+  };
+
   return(
     <Container>
       <h1>Available Page</h1>
