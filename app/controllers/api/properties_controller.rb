fec59abd2153b06e194b3cd7d8fa48a9981464a9
Michael Knutson <mrknutson44@gmail.com>
2022-01-06 09:40:52 -0900

    cities backend working

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -4,4 +4,12 @@ class Api::PropertiesController < ApplicationController
     render json: Property.available
   end
 
+  def get_cities
+    render json: Address.get_cities
+  end
+
+  def city
+    render json: Property.by_city(params[:city])
+  end
+
 end
